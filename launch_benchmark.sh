61a13ccc3c37fb769e13c9aa8ae65c05ac6981bd
jundu <jun.du@intel.com>
2023-05-14 18:34:23 -0700

    update yolov5 requirement

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -9,6 +9,7 @@ function main {
     set_environment
 
     # requirements
+    pip install ipython==8.8.0
 
     # if multiple use 'xxx,xxx,xxx'
     model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
