f47ac2e6969a72c31d4d199aefe934027dea5178
jundu <jun.du@intel.com>
2023-05-14 19:19:13 -0700

    update requirement

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -9,7 +9,7 @@ function main {
     set_environment
 
     # requirements
-    pip install ipython==8.8.0
+    pip install ipython==8.8.0 seaborn==0.12.2
 
     # if multiple use 'xxx,xxx,xxx'
     model_name_list=($(echo "${model_name}" |sed 's/,/ /g'))
