3df600860f9f75a29721edf48c9029b4db7d2df8
jun.du@intel.com <jun.du@intel.com>
2024-05-27 23:09:41 -0700

    update

--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -60,9 +60,6 @@ function generate_core {
             OOB_EXEC_HEADER+=" -C $(echo ${device_array[i]} |awk -F ';' '{print $1}') "
         elif [ "${device}" == "cuda" ];then
             OOB_EXEC_HEADER=" CUDA_VISIBLE_DEVICES=${device_array[i]} "
-            if [[ "${mode_name}" == "realtime" ]];then
-                addtion_options+=" --nv_fuser "
-            fi
         elif [ "${device}" == "xpu" ];then
             OOB_EXEC_HEADER=" ZE_AFFINITY_MASK=${i} "
         fi
