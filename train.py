94fcbc3248bf317169ba6a4d214ab372dd6b7f2f
Zhong, Ruijie <ruijie.zhong@intel.com>
2024-12-11 01:02:24 -0800

    update channel last

--- train.py
+++ train.py
@@ -507,7 +507,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                 callbacks.run('on_train_batch_start')
                 ni = i + nb * epoch  # number integrated batches (since train start)
 
-                if opt.channels_last and opt.device_str != "xpu":
+                if opt.channels_last:
                     imgs = imgs.to(memory_format=torch.channels_last)
                 start_time = time.time()
                 imgs = imgs.to(device, non_blocking=True).float() / 255  # uint8 to float32, 0-255 to 0.0-1.0
